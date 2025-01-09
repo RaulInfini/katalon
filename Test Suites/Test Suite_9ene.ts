04533f5466fff65ecf0e21f8c50ca6018ed38b42
Raúl Izquierdo <163156968+RaulInfini@users.noreply.github.com>
2025-01-09 17:58:38 +0100

    initial commit katalon

--- Test Suites/Test Suite_9ene.ts
+++ Test Suites/Test Suite_9ene.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>244213f3-8b8d-4b2a-ae0b-5c7f927450cf</testSuiteGuid>
+   <testCaseLink>
+      <guid>2ba1e5cf-9d34-407a-a6e6-81fb64d22468</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test_9ene</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
