aa0741e3ce96507ae8e95ec3b1fd915b9fd5599a
Raúl Izquierdo <163156968+RaulInfini@users.noreply.github.com>
2025-01-09 17:57:24 +0100

    init commit katalon test

---
+++ Test Suites/Test Suite_9ene.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite_9ene</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>244213f3-8b8d-4b2a-ae0b-5c7f927450cf</testSuiteGuid>
+</TestSuiteEntity>
